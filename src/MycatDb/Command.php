e99a4da9a05bce455557c36004cb416fb82dd69b
heaviii <lihw@knowbox.cn>
2019-03-01 14:55:06 +0800

    command

--- src/MycatDb/Command.php
+++ src/MycatDb/Command.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: heavi
- * Date: 2019/2/20
- * Time: 10:26 AM
- */
 
 namespace Knowbox\Mycat;
 
@@ -45,11 +39,51 @@ class Command extends \yii\db\Command
         return $this;
     }
 
+    /**
+     * Returns the raw SQL by inserting parameter values into the corresponding placeholders in [[sql]].
+     * Note that the return value of this method should mainly be used for logging purpose.
+     * It is likely that this method returns an invalid SQL due to improper replacement of parameter placeholders.
+     * @return string the raw SQL with parameter values inserted into the corresponding placeholders in [[sql]].
+     */
+    public function getRawSql()
+    {
+        if (empty($this->params)) {
+            $sql = $this->_sql;
+            \Yii::info($sql, 'sqlLogFile');
+            return $sql;
+        }
+        $params = [];
+        foreach ($this->params as $name => $value) {
+            if (is_string($name) && strncmp(':', $name, 1)) {
+                $name = ':' . $name;
+            }
+            if (is_string($value)) {
+                $params[$name] = $this->db->quoteValue($value);
+            } elseif (is_bool($value)) {
+                $params[$name] = ($value ? 'TRUE' : 'FALSE');
+            } elseif ($value === null) {
+                $params[$name] = 'NULL';
+            } elseif (! is_object($value) && ! is_resource($value)) {
+                $params[$name] = $value;
+            }
+        }
+        if (! isset($params[1])) {
+            $sql = strtr($this->_sql, $params);
+            \Yii::info($sql, 'sqlLogFile');
+            return $sql;
+        }
+        $sql = '';
+        foreach (explode('?', $this->_sql) as $i => $part) {
+            $sql .= (isset($params[$i]) ? $params[$i] : '') . $part;
+        }
+        \Yii::info($sql, 'sqlLogFile');
+        return $sql;
+    }
+
     private function getTraceIdSql($sql)
     {
         $traceId = $_SERVER['X-Request-Id']??'';
-        $sql = empty($traceId) ? $sql : "/*traceId={$traceId}*/ " . $sql;
-        \Yii::info($sql, 'sqlLogFile');
+        $sql = empty($traceId) ? $sql : "/* traceId={$traceId} */ " . $sql;
         return $sql;
     }
 
